72a7a25ea1cdad43a76e35d989f38890e935b16a
Tomasz Kania <tomasz.kania@mamf.pl>
2017-01-04 22:08:22 +0100

    pierwsze funkcje na js

---
+++ script.js
@@ -0,0 +1,17 @@
+//hiragana z odpowiednim tłumaczeniem
+var slownik={
+
+};
+
+
+//losowanie znaków hiragany
+function losowanie(){
+
+
+
+}
+//sprawdzanie z funkcji losującej do odpowiedzi zapisanej przez użytkownika
+function sprawdzanie(){
+
+
+}
