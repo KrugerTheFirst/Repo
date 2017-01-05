6cc09481a22b2b4cf1c182762cf9728ab2d5f5a9
Kruger <r.r.s.orzel@gmail.com>
2017-01-05 21:13:02 +0100

    Wrzucenie tablicy z hiraganą

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
 //hiragana z odpowiednim tłumaczeniem
-var slownik={
-
-};
+var slownik = {};
+slownik['あ'] = 'a';
+slownik['い'] = 'i';
+slownik['う'] = 'u';
+slownik['え'] = 'e';
+slownik['お'] = 'o';
+slownik['か'] = 'ka';
+slownik['き'] = 'ki';
+slownik['く'] = 'ku';
+slownik['け'] = 'ke';
+slownik['こ'] = 'ko';
+slownik['さ'] = 'sa';
+slownik['し'] = 'shi';
+slownik['す'] = 'su';
+slownik['せ'] = 'se';
+slownik['そ'] = 'so';
+slownik['た'] = 'ta';
+slownik['ち'] = 'chi';
+slownik['つ'] = 'tsu';
+slownik['て'] = 'te';
+slownik['と'] = 'to';
+slownik['な'] = 'na';
+slownik['に'] = 'ni';
+slownik['ぬ'] = 'nu';
+slownik['ね'] = 'ne';
+slownik['の'] = 'no';
+slownik['は'] = 'ha';
+slownik['ひ'] = 'hi';
+slownik['ふ'] = 'fu';
+slownik['へ'] = 'he';
+slownik['ほ'] = 'ho';
+slownik['ま'] = 'ma';
+slownik['み'] = 'mi';
+slownik['む'] = 'mu';
+slownik['め'] = 'me';
+slownik['も'] = 'mo';
+slownik['や'] = 'ya';
+slownik['ゆ'] = 'yu';
+slownik['よ'] = 'yo';
+slownik['ら'] = 'ra';
+slownik['り'] = 'ri';
+slownik['る'] = 'ru';
+slownik['れ'] = 're';
+slownik['ろ'] = 'ro';
+slownik['わ'] = 'wa';
+slownik['を'] = 'wo';
+slownik['ん'] = 'n';
 
 
 //losowanie znaków hiragany
 //tip 1)iteracja obiektów js
 // 2)math.random
 //W danym zakresie random (zmiana)
-function losowanie(){
-
+function losowanie() {
 
 
 }
 //sprawdzanie z funkcji losującej do odpowiedzi zapisanej przez użytkownika
-function sprawdzanie(){
+function sprawdzanie() {
 
 
 }
